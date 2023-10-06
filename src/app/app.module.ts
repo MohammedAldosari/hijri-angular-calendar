bf22b48077d9ec65eca8676ae4a5b0fd8ea44def
Mohammed Aldosari <matrix2526@gmail.com>
2023-10-06 18:45:35 +0300

    fix issue with translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+    return new TranslateHttpLoader(http, './assets/i18n/');
 }
 
 @NgModule({
